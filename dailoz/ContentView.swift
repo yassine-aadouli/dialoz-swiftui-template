12089c928eddd958537e9474084c1028f1949edd
Appslix <mr.aadouli@gmail.com>
2021-12-28 14:38:29 +0100

    Initial Commit

---
+++ dailoz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dailoz
+//
+//  Created by Yassine AADOULI on 28/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dailoz/dailozApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailozApp.swift
+//  dailoz
+//
+//  Created by Yassine AADOULI on 28/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct dailozApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
